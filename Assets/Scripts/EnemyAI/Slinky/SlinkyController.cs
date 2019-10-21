c89171d0465de63099a1fba07ae2e71ec08d07f1
Knabb <Emil.N.Karlsson@gmail.com>
2019-10-21 14:30:03 +0200

    Added a new enemy + script to the game, he slinky!

---
+++ Assets/Scripts/EnemyAI/Slinky/SlinkyController.cs
@@ -0,0 +1,126 @@
+﻿using AirTowerDefence.EnemySpawn;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AirTowerDefence.Enemy.Controllers
+{
+    public class SlinkyController : Controller
+    {
+        private Animator _Animator;
+
+        [SerializeField]
+        private Transform _ParentTransform;
+
+        [SerializeField]
+        private Transform _AnimationSteps;
+
+        [SerializeField]
+        private float TimeBetweenHops;
+
+        [SerializeField]
+        private SquareWaypoint TargetWayPoint;
+
+        private Vector3 _TargetPositionInWorldSpace;
+        private Vector3 _RelativePositionToWayPoint;
+
+        private Vector3 _NewPosition;
+        [SerializeField]
+        private Quaternion _NewRotation;
+
+        private float HopTimer;
+
+        [SerializeField]
+        private float Distance;
+
+        void Start()
+        {
+            FindSpawnPoint();
+
+            _NewPosition = _ParentTransform.position;
+            HopTimer = TimeBetweenHops;
+
+            _TargetPositionInWorldSpace = transform.position;
+            _RelativePositionToWayPoint = TargetWayPoint.transform.InverseTransformPoint(transform.position);
+        }
+
+        private void FindSpawnPoint()
+        {
+            TargetWayPoint = GameObject.FindGameObjectWithTag("Spawnpoint").GetComponent<SquareWaypoint>();
+        }
+
+        private void Awake()
+        {
+            _Animator = GetComponent<Animator>();
+        }
+
+        public override void UpdateControl()
+        {
+
+            HopTimer -= Time.deltaTime;
+
+            if (HopTimer <= 0f)
+            {
+                HopForward();
+                HopTimer = TimeBetweenHops;
+            }
+        }
+
+        private void HopForward()
+        {
+            //Play again from new position
+            _ParentTransform.position = _NewPosition;
+            _ParentTransform.rotation = _NewRotation;
+            _Animator.Play("Slinky_HopForward", 0, 0f);
+        }
+
+        public void OnMiddleStepReached()
+        {
+            Debug.Log("MiddleStep");
+        }
+
+        public void OnAnimationEnd()
+        {
+            StoreNewPosition();
+
+            Distance = Vector3.Distance(_NewPosition, _TargetPositionInWorldSpace);
+
+            if (IsCloseToTargetDestination())
+            {
+                GetNextTargetPosition();
+                StoreNewRotation();
+            }
+        }
+
+        private bool IsCloseToTargetDestination()
+        {
+            return Vector3.Distance(_NewPosition, _TargetPositionInWorldSpace) < 4f;
+        }
+
+        private void GetNextTargetPosition()
+        {
+            if (TargetWayPoint.Next != null)
+            {
+                TargetWayPoint = TargetWayPoint.Next;
+            }
+
+            _TargetPositionInWorldSpace = TargetWayPoint.transform.TransformPoint(_RelativePositionToWayPoint);
+        }
+
+        private void StoreNewPosition()
+        {
+            _NewPosition = _AnimationSteps.position;
+        }
+        private void StoreNewRotation()
+        {
+            _NewRotation = GetNewRotation();
+        }
+
+        private Quaternion GetNewRotation()
+        {
+            Quaternion lookrot = Quaternion.LookRotation(_TargetPositionInWorldSpace - _NewPosition);
+            return Quaternion.Euler(0, lookrot.eulerAngles.y, 0);
+        }
+    }
+}
