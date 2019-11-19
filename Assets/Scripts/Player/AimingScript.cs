e2083147914ba52374104f8c87a6fd7af19ea3fa
Emil <47690578+Knubb@users.noreply.github.com>
2019-11-20 00:28:28 +0100

    Added Aiming Basic function
    Aiming marker follows the mouse input on the ground.

---
+++ Assets/Scripts/Player/AimingScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimingScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject AimMarker;
+
+    void Start()
+    {
+
+        //AimMarker.GetComponent<Collider>().enabled = false;
+        AimMarker.transform.localScale = Vector3.one / 3;
+    }
+
+    void Update()
+    {
+        this.RayCastFromCamera();
+    }
+
+
+    private void RayCastFromCamera()
+    {
+        Ray ray = new Ray(this.transform.position, this.transform.position + transform.forward * 1000f);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 10000f, LayerMask.GetMask("Terrain")))
+        {
+            AimMarker.transform.position = hit.point;
+            AimMarker.transform.position = new Vector3(hit.point.x, hit.point.y + 2f, hit.point.z);
+        }
+    }
+}
