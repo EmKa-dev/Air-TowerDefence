63e113cf9e15c869689dad6c4983fe937cf68f73
Knabb <Emil.N.Karlsson@gmail.com>
2019-11-21 18:05:48 +0100

    EDge-Glow shader added.

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _Player;
+
+    [SerializeField]
+    private float _LerpTime;
+
+    [SerializeField]
+    private Vector3 _Offset;
+
+    void Start () {
+
+        _Offset = transform.position - _Player.transform.position;
+
+	}
+	
+	void LateUpdate () 
+    {
+        transform.position = Vector3.Lerp(transform.position, _Player.transform.position + _Offset, _LerpTime * Time.deltaTime);
+    }
+}
