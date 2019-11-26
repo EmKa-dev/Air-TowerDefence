b2db0cc3bfc631dbb4a1379da2a9cece656bdc4e
Knabb <Emil.N.Karlsson@gmail.com>
2019-11-26 16:21:31 +0100

    Changes to AimingScript.
    Changed size of aiming marker reticle.

--- Assets/Scripts/Player/AimingScript.cs
+++ Assets/Scripts/Player/AimingScript.cs
@@ -7,11 +7,16 @@ public class AimingScript : MonoBehaviour
     [SerializeField]
     private GameObject AimMarker;
 
+    private GameObject _Marker;
+
+    private int _LayerMask;
+
     void Start()
     {
-
+        _LayerMask = LayerMask.GetMask("Terrain");
         //AimMarker.GetComponent<Collider>().enabled = false;
-        AimMarker.transform.localScale = Vector3.one / 3;
+        _Marker = Instantiate(AimMarker);
+        _Marker.transform.localScale = Vector3.one / 2;
     }
 
     void Update()
@@ -24,10 +29,10 @@ public class AimingScript : MonoBehaviour
     {
         Ray ray = new Ray(this.transform.position, this.transform.position + transform.forward * 1000f);
 
-        if (Physics.Raycast(ray, out RaycastHit hit, 10000f, LayerMask.GetMask("Terrain")))
+        if (Physics.Raycast(ray, out RaycastHit hit, 10000f, _LayerMask))
         {
-            AimMarker.transform.position = hit.point;
-            AimMarker.transform.position = new Vector3(hit.point.x, hit.point.y + 2f, hit.point.z);
+            _Marker.transform.position = hit.point;
+            _Marker.transform.position = new Vector3(hit.point.x, hit.point.y + 0.3f, hit.point.z);
         }
     }
 }
