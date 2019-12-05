242cd26ccc705e598a8c5b10f4774d3a80a67763
Emil <47690578+Knubb@users.noreply.github.com>
2019-12-05 12:16:22 +0100

    Added a functional camera script (Appears jittery during movement)

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -1,24 +1,68 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
-    [SerializeField]
-    private GameObject _Player;
-
-    [SerializeField]
-    private float _LerpTime;
 
     [SerializeField]
-    private Vector3 _Offset;
+    private Transform _Target;
 
-    void Start () {
+    [SerializeField]
+    private Vector3 _OffsetPosition;
 
-        _Offset = transform.position - _Player.transform.position;
+    [SerializeField]
+    private bool _Damping;
+    [SerializeField]
+    private float _DampTime;
 
-	}
-	
-	void LateUpdate () 
+    private Vector3 _DesiredPosition;
+
+    private void Start()
     {
-        transform.position = Vector3.Lerp(transform.position, _Player.transform.position + _Offset, _LerpTime * Time.deltaTime);
+        if (_Target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+
+            return;
+        }
+    }
+
+    void Update()
+    {
+        CalculatePosition();
+    }
+
+    private void LateUpdate()
+    {
+        MoveToPosition();
+    }
+
+    private void MoveToPosition()
+    {
+        transform.position = _DesiredPosition;
+    }
+
+    public void CalculatePosition()
+    {
+        // compute position
+        Quaternion rot = Quaternion.Euler(_Target.transform.eulerAngles.x, _Target.transform.eulerAngles.y, 0);
+
+        if (_Damping)
+        {
+            _DesiredPosition = Vector3.MoveTowards(transform.position, _Target.transform.position + (rot * _OffsetPosition), _DampTime * Time.deltaTime);
+
+            //transform.position = Vector3.Lerp(transform.position, _Target.transform.position + (rot * _OffsetPosition), _LerpTime * Time.deltaTime);
+        }
+        else
+        {
+            _DesiredPosition = _Target.transform.position + (rot * _OffsetPosition);
+        }
+
+        // compute rotation
+
+        transform.rotation = _Target.rotation;
+
+        
     }
 }
